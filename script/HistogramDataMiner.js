a0a697f6c9a5a5540339e6d16a2930f09b3057aa
Isabelly <icfd@cin.ufpe.br>
2017-05-26 15:07:25 -0300

    finished getting data

--- script/HistogramDataMiner.js
+++ script/HistogramDataMiner.js
@@ -1,8 +1,50 @@
-d3.json("result.json", function(error, json){
-    alert(console.error();)
+var allLibs = []
 
-    if error != null {
-      console.log(json);
+function addLibToAllLibs (name, frequency){
+  var didEnter = false
+  for (var i = 0; i < allLibs.length; i++){
+    if (allLibs[i]["name"] === name) {
+      allLibs[i]["frequency"] += frequency
+      didEnter = true
+      break
     }
+  }
 
-})
+  if (!didEnter) {
+    var object = {
+      name: name,
+      frequency: frequency
+    }
+    allLibs.push(object)
+  }
+}
+
+function loadAllLibs() {
+  return new Promise(  (fulfill, reject) => {
+    d3.json('result.json', (error, json) => {
+      if (error) {
+        throw reject(error)
+      }
+      var keysArray = Object.keys(json)
+
+      for ( var i = 0; i < keysArray.length;  i++) {
+          console.log(keysArray[i]);
+          var libsForProjectDictionary = json[keysArray[i]]["libs"]
+
+          var libNames = Object.keys(libsForProjectDictionary)
+
+          for (var j =0 ; j < libNames.length; j++ ){
+            const name = libNames[j]
+            const frequency = libsForProjectDictionary[name]
+
+            addLibToAllLibs(name, frequency)
+          }
+      }
+      fulfill(allLibs)
+    })
+  })
+}
+
+// loadAllLibs().then(result => {
+//   console.log(result);
+// })
