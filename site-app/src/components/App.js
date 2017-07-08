a51af9b9c6eb160fc0a2c918de87763a6c803eab
Isabelly <icfd@cin.ufpe.br>
2017-07-08 14:52:42 -0300

    Deleting unused code

--- site-app/src/components/App.js
+++ site-app/src/components/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react'
 import NavBar from './NavBar'
 import Content from './Content'
 import Footer from './Footer'
-import Visualization from './Visualization'
 import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 
 class App extends Component {
 
@@ -14,13 +11,7 @@ class App extends Component {
 
             <div className='app-flex'>
                 <NavBar/>
-                <main>
-                    {/*<Router>
-                        <div>
-                         <Route exact path="/" component={Content}/>
-                         <Route path="/visulizations" component={Visualization}/>
-                         </div>
-                    </Router>*/}
+                <main>      
                     <Content/>
                 </main>
                 <Footer/>
