5fca7d29cbb79f7045aa0cd26caa632743cd1092
Isabelly <icfd@cin.ufpe.br>
2017-07-08 12:44:37 -0300

    Added router to show visualization

---
+++ site-app/src/components/Visualization.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Visualization extends Component {
+    render() {
+        return(
+            <div>OI</div>
+        )
+    }
+}
+
+export default Visualization;
