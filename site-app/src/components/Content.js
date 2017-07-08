e2da15083db1a6ee645206fceb857b0d88e801ae
Isabelly <icfd@cin.ufpe.br>
2017-07-07 22:50:36 -0300

    Added Links normal, and a blockquote in about

--- site-app/src/components/Content.js
+++ site-app/src/components/Content.js
@@ -8,8 +8,19 @@ class Content extends Component {
                     <div className='col s7 offset-s2'>
                         <h3>About</h3>
                         <div className='justify-text'>
+                            <blockquote >
                             <p>This projects aims provide a platform which allows the analisys of Swift projects repositories from Students of a extension project at Center of Informatics - UFPE.</p>
                             <p>Our goal is to provide a better understating of how knowledge is disseminated between students of this project; Allowing to easily find framework specialists among them.</p>
+                            </blockquote>
+                        </div>
+                    </div>  
+                </div> 
+                <div className='row'>
+                    <div className='col s7 offset-s2'>
+                        <h3>Links</h3>
+                        <div className="collection">
+                            <a href="https://github.com/FilipeJrd/Visualizacao-Alunos/raw/master/Presentations/proposta.pdf" className="collection-item light-blue-text text-darken-2">Proposal Presentation</a>
+                            <a href="#!" className="collection-item light-blue-text text-darken-2">Demo (work-in-progress)</a>
                         </div>
                     </div>  
                 </div> 
